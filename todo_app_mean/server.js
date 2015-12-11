6644913fee8cba2853ec4327b8fdf22af8622574
Jeffrey Macks <jmacks1988@gmail.com>
2015-12-11 14:45:52 -0500

    initial setup and commit

---
+++ todo_app_mean/server.js
@@ -0,0 +1,33 @@
+'use strict';
+//required globally
+const express = require('express');
+let app = express();
+
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const expressJWT = require('express-jwt');
+const jwt = require('jsonwebtoken');
+
+const routes = require('./routes/todo.js');
+
+//stuff to use
+app.use('/', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlendoded({ extended: false }));
+app.use(logger('dev'));
+app.use('/scripts', express.static(__dirname + '/node_modules'));
+app.use(routes);
+//connection to the mongo database
+mongoose.connect('mongodb://localhost/todo', function(err){
+  if(err){
+    console.log('DATABASE CONNECTION ERROR!')
+  } else {
+    console.log('DATABASE CONNECTED')
+  }
+});
+const db = mongoose.connection;
+//now listen to the server
+const server = app.listen(3000, function(){
+  console.log('TODO RUNNING ON PORT 3000')
+});
